fc145f911b65b88ced6fb0426dd38afa55a04845
ISST-24 <79929675+ISST-24@users.noreply.github.com>
2021-04-10 20:30:32 +0200

    Delete demo directory

--- demo/src/main/java/com/example/demo/repository/SubjectCalificacion.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-public interface SubjectCalificacion {
-
-	String getName();
-	Double getNota();
-	Boolean getDefinitiva();
-	
-}
--- demo/src/main/java/com/example/demo/service/SubjectService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.response.GetSubjectsByIdResponse;
-
-public interface SubjectService {
-	
-	 GetSubjectsByIdResponse getSubjectsById(String id);
-
-}
