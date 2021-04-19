d38e28e80b42c618647f0a567ea21db8d06b4b64
RubenTG <80395096+RubenTG@users.noreply.github.com>
2021-04-19 17:02:38 +0200

    ISSTscript.js de Sanz

--- js/ISSTscript.js
+++ js/ISSTscript.js
@@ -4,28 +4,55 @@ function uploadMarks() {
         if (typeof (FileReader) != "undefined") {
             var reader = new FileReader();
             reader.onload = function (e) {
-                Papa.parse(reader.result,{
-                    header : true,
-                    complete : function(result) {
-                        console.log(result);
-                        var marksTable = $("#marksTable").DataTable({
-                            data : result.data,
-                            columns: [
-                                { data: 'Nombre'},
-                                { data: 'Nota'},
-                            ]
-                        });
-                        
-                    } 
+                var allMarks=[]
+                var table = $("<table class='table table-bordered' id='dataTable' width='100%' cellspacing='0'/>");
+                var rows = e.target.result.split("\n");
+                for (var i = 0; i < rows.length; i++) {
+                    if(i===0){
+                        var row = $("<tr class='table-primary' />");
+                        var cells = rows[i].split(",");
+                        if (cells.length > 1) {
+                            for (var j = 0; j < cells.length; j++) {
+                                var cell = $("<th />");
+                                cell.html(cells[j]);
+                                row.append(cell);
+                            }
+                            table.append(row);
+                        }
+                    }else{
+                        var markJSON={}
+                        var row = $("<tr />");
+                        var cells = rows[i].split(",");
+                        if (cells.length > 1) {
+                            for (var j = 0; j < cells.length; j++) {
+                                const title= rows[0].split(",")[j]
+                                const value= cells[j]
+                                markJSON[title]=value
+                                var cell = $("<td />");
+                                cell.html(cells[j]);
+                                row.append(cell);
+                            }
+                            table.append(row);
+                            allMarks.push(markJSON);
+                        }
+                    }
+
                 }
-                    
-                );
+                $("#marksTable").html('')
+                $("#marksTable").append(table);
+               //console.log("allMarks",allMarks)
+                var FinalJSON = JSON.stringify(rows);
+               //var marksTable = $("#marksTable").dataTable;
+                //marksTable.rows.add(data).draw();
+                console.log(FinalJSON);
             }
+            reader.readAsText($("#marksCSV")[0].files[0]);
+            
+
         } else {
             alert("This browser does not support HTML5.");
         }
     } else {
         alert("Please upload a valid CSV file.");
     }
-
 }
