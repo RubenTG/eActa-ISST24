93e5a8e7290be6fc772c89d787e85db772fc1101
RubenTG <ruben.triguero.garcia@alumnos.upm.es>
2021-05-05 13:34:36 +0200

    ISSTscript con papaparser
    Prueba de uploadMarks con papaparser

--- js/ISSTscript.js
+++ js/ISSTscript.js
@@ -2,53 +2,43 @@ function uploadMarks() {
     var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
     if (regex.test($("#marksCSV").val().toLowerCase())) {
         if (typeof (FileReader) != "undefined") {
+            var file = document.getElementById('marksCSV').files[0];
+            console.log(file);
             var reader = new FileReader();
+            console.log(reader);
+            reader.readAsText(file);
             reader.onload = function (e) {
-                var allMarks=[]
-                var table = $("<table class='table table-bordered' id='dataTable' width='100%' cellspacing='0'/>");
-                var rows = e.target.result.split("\n");
-                for (var i = 0; i < rows.length; i++) {
-                    if(i===0){
-                        var row = $("<tr class='table-primary' />");
-                        var cells = rows[i].split(",");
-                        if (cells.length > 1) {
-                            for (var j = 0; j < cells.length; j++) {
-                                var cell = $("<th />");
-                                cell.html(cells[j]);
-                                row.append(cell);
-                            }
-                            table.append(row);
-                        }
-                    }else{
-                        var markJSON={}
-                        var row = $("<tr />");
-                        var cells = rows[i].split(",");
-                        if (cells.length > 1) {
-                            for (var j = 0; j < cells.length; j++) {
-                                const title= rows[0].split(",")[j]
-                                const value= cells[j]
-                                markJSON[title]=value
-                                var cell = $("<td />");
-                                cell.html(cells[j]);
-                                row.append(cell);
-                            }
-                            table.append(row);
-                            allMarks.push(markJSON);
-                        }
-                    }
+                Papa.parse(reader.result,{
+                    header : true,
+                    skipEmptyLines : true,
+                    complete : function(result) {
+                        console.log(result);
+                        var marksTable = $("#marksTable").DataTable({
+                            data : result.data,
+                            columns: [
+                                { data: 'Nombre'},
+                                { data: 'Nota'},
+                                { data: 'Definitiva',
+                                render: function ( data, type, row, meta ) {
+                                    if(data === 'true'){
+                                        return '<button type="button" class="btn btn-primary" disabled onClick="searchRevisionStudents()">Revisar</button>';
 
+                                    }else{
+                                     return '<button type="button" class="btn btn-primary" onClick="searchRevisionStudents()">Revisar</button>';
+                                   }
+                                }
+                                }
+                            ]
+                        });
+
+                    } 
                 }
-                $("#marksTable").html('')
-                $("#marksTable").append(table);
-               //console.log("allMarks",allMarks)
-                var FinalJSON = JSON.stringify(rows);
-               //var marksTable = $("#marksTable").dataTable;
-                //marksTable.rows.add(data).draw();
-                console.log(FinalJSON);
-            }
-            reader.readAsText($("#marksCSV")[0].files[0]);
-            
 
+                );
+            }
+            reader.onerror = function() {
+                console.log(reader.error);
+              };
         } else {
             alert("This browser does not support HTML5.");
         }
